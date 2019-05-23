5360abc1d88d7b1a44b58afdc96beb2b95091b77
rogerio ribeiro <rogerio.ribeiro@fielo.com>
2019-05-23 16:43:06 -0300

    change code using mongodb

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/api/v1/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/api/v1/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
